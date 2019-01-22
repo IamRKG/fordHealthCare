cfd29dee0117362448871f9a7d70269222c3a56f
Gopal <GRADHAK3@ford.com>
2019-01-22 14:53:50 +0530

    modified

--- src/app.js
+++ src/app.js
@@ -7,7 +7,22 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-mongoose.connect(config.getDBConnectionString(),{useNewUrlParser: true})
+const options = {
+    useMongoClient: true,
+    autoIndex: false, // Don't build indexes
+    reconnectTries: 10000, // Never stop trying to reconnect
+    reconnectInterval: 500, // Reconnect every 500ms
+    poolSize: 10, // Maintain up to 10 socket connections
+    // If not connected, return errors immediately rather than waiting for reconnect
+    bufferMaxEntries: 0,
+    useNewUrlParser: true
+};
+
+mongoose.connect(config.getDBConnectionString(), options).then( () =>{
+    console.log("Connected")
+},(error)=>{
+    console.log("err",error);
+})
 
 routers(app)
 
